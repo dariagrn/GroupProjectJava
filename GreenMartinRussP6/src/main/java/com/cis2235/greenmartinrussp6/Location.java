d44d68535fbcdc4e83e45a427271af29e77474a0
Jerrell Russ <jruss1@cnm.edu>
2024-10-29 22:43:41 -0700

    Added the stuff for Location

--- GreenMartinRussP6/src/main/java/com/cis2235/greenmartinrussp6/Location.java
+++ GreenMartinRussP6/src/main/java/com/cis2235/greenmartinrussp6/Location.java
@@ -4,8 +4,31 @@
 package com.cis2235.greenmartinrussp6;
 
 public class Location {
+    private String name;
+    private String description;
 
+    public Location(String name){
+        this.name = name;
+    }
 
+    public Location[] exits;    //exits is an array of Locations that this location connects to
 
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        description = "You are standing in the " + name
+                + ".  \r\nYou see exits to the following places: ";
+
+        for(int i = 0; i < exits.length; i++)
+        {
+            description += "\r\n " + exits[i].getName();
+            if( i!= exits.length - 1)
+                description += ",";
+        }
+        description += ".\r\n";
+        return description;
+    }
 
 }
